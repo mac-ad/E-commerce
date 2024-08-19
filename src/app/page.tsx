45cba9028a854e7fc4a3f172ae205af4271d7b41
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-08-19 10:32:59 +0545

    footer design completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <Navbar/>
+      <Navbar />
+      <Footer />
     </main>
   );
 }
