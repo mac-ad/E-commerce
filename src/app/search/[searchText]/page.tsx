f85cee5a3cbb6ccd828b6474a3052a8dbb851333
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-09-05 15:30:53 +0545

    finally searchedoutput resulted

--- src/app/search/[searchText]/page.tsx
+++ src/app/search/[searchText]/page.tsx
@@ -1,29 +1,50 @@
-import { fetchTVCollection, Products } from "@/app/components/TvCollection";
-import Spinner from "@/app/ui/Spinner";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import React from "react";
+"use client";
+import { Products } from "@/app/components/TvCollection";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function SearchResult() {
-  const {
-    data: search,
-    isLoading,
-    isError,
-  }: UseQueryResult<Products[], Error> = useQuery({
-    queryKey: ["search"],
-    queryFn: fetchTVCollection,
-  });
+export default function GetResultBySearch() {
+  const params = useParams();
+  const [searchedResult, setSearchResult] = useState([]);
+  console.log(params.searchText);
 
-  if (isLoading) {
-    return (
-      <div className="w-full bg-gray-100 p-3 min-h-screen flex justify-center items-center">
-        <Spinner />
-      </div>
-    );
+  useEffect(()=>{
+    fetchProducts();
+  }, [])
+  const fetchProducts = async() =>{
+    const data = await fetch('/api/products');
+    if(!data.ok){
+        throw new Error("Data is not okay");
+    }
+    const result = await data.json();
+    return setSearchResult(result)
   }
 
-  if (isError || !search) {
-    return <div>Error fetching members</div>;
-  }
+  const result = searchedResult.map((result:Products)=> result)
+  const searchedOutput = result.filter((output)=>output.name.toLowerCase().includes(params.searchText.toLowerCase()))
+  console.log(searchedOutput)
+
+  console.log(searchedResult)
+//   const {
+//     data: search,
+//     isLoading,
+//     isError,
+//   }: UseQueryResult<Products[], Error> = useQuery({
+//     queryKey: ["search"],
+//     queryFn: fetchTVCollection,
+//   });
+
+//   if (isLoading) {
+//     return (
+//       <div className="w-full bg-gray-100 p-3 min-h-screen flex justify-center items-center">
+//         <Spinner />
+//       </div>
+//     );
+//   }
+
+//   if (isError || !search) {
+//     return <div>Error fetching members</div>;
+//   }
   return (
     <>
       <div className="pt-[130px]">
