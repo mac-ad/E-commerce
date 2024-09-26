94a8af1cef2ce0193912b6617570a5fa0f65f72a
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-09-26 19:00:41 +0545

    working on price range

--- src/app/AC/page.tsx
+++ src/app/AC/page.tsx
@@ -7,7 +7,7 @@ import {
 } from "../components/CatProdDetailCard";
 import { Products } from "../components/TvCollection";
 import Spinner from "../ui/Spinner";
-import { FaAngleDown } from "react-icons/fa";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
 export default function AirConditioner() {
   const pathname = usePathname();
@@ -15,6 +15,14 @@ export default function AirConditioner() {
 
   const [productType, setProductType] = useState<Products[]>([]);
   const [filteredProduct, setFilteredProduct] = useState<Products[]>([]);
+  const [priceRange, setPriceRange] = useState([0, 5000])
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [visibility, setVisibility] = useState({
+    shopBy: false,
+    categories: false,
+    sizes: false,
+  });
 
   const fetchDataByCategory = async () => {
     try {
@@ -24,7 +32,7 @@ export default function AirConditioner() {
       }
       const response = await data.json();
       setProductType(response.products);
-      setFilteredProduct(response.products)
+      setFilteredProduct(response.products);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
@@ -34,7 +42,7 @@ export default function AirConditioner() {
     fetchDataByCategory();
   }, [type]);
 
-  if (filteredProduct.length == 0) {
+  if (filteredProduct.length === 0) {
     return (
       <div className="w-full bg-gray-100 p-3 min-h-screen flex justify-center items-center">
         <Spinner />
@@ -43,11 +51,21 @@ export default function AirConditioner() {
   }
 
   const fetchByCategory = (category: string) => {
-    const data = productType.filter((product) => product.category == category);
-    return setFilteredProduct(data);
+    const data = productType.filter((product) => product.category === category);
+    setFilteredProduct(data);
   };
 
-  console.log(filteredProduct)
+  const fetchBySize = (size: number) => {
+    const data = productType.filter((product) => product.size === size);
+    setFilteredProduct(data);
+  };
+
+  const toggleVisibility = (section: keyof typeof visibility) => {
+    setVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [section]: !prevVisibility[section],
+    }));
+  };
 
   return (
     <div className="w-full pt-[130px] bg-gray-100 flex justify-center">
@@ -56,38 +74,72 @@ export default function AirConditioner() {
           <div className="lg:col-span-1 md:col-span-1">
             <div className="bg-white py-2 px-3 border border-gray-200 flex justify-between">
               <h1 className="font-semibold text-sm">Shop By</h1>
-              <p>
-                <FaAngleDown />
+              <p onClick={() => toggleVisibility("shopBy")}>
+                {visibility.shopBy ? <FaAngleUp /> : <FaAngleDown />}
               </p>
             </div>
-            <div className="bg-white border border-gray-200">
-              <p
-                className="text-xs py-2 px-6 cursor-pointer hover:bg-gray-200"
-                onClick={() => fetchDataByCategory()}
-              >
-                {type}
-              </p>
-            </div>
-            <div className="bg-white py-2 px-3 border border-gray-200 flex justify-between">
-              <h1 className="font-semibold text-sm">Categories</h1>
-              <p>
-                <FaAngleDown />
-              </p>
-            </div>
-            <div className="bg-white border border-gray-200">
-              {Array.from(
-                new Set(productType.map((product) => product.category))
-              ).map((category) => (
+            {!visibility.shopBy && (
+              <div className="bg-white border border-gray-200">
                 <p
                   className="text-xs py-2 px-6 cursor-pointer hover:bg-gray-200"
-                  key={category}
-                  onClick={() => fetchByCategory(category)}
+                  onClick={() => fetchDataByCategory()}
                 >
-                  {category}
+                  {type}
                 </p>
-              ))}
+              </div>
+            )}
+
+            <div className="bg-white py-2 px-3 border border-gray-200 flex justify-between">
+              <h1 className="font-semibold text-sm">Categories</h1>
+              <p onClick={() => toggleVisibility("categories")}>
+                {visibility.categories ? <FaAngleUp /> : <FaAngleDown />}
+              </p>
+            </div>
+            {!visibility.categories && (
+              <div className="bg-white border border-gray-200">
+                {Array.from(
+                  new Set(productType.map((product) => product.category))
+                ).map((category) => (
+                  <p
+                    className="text-xs py-2 px-6 cursor-pointer hover:bg-gray-200"
+                    key={category}
+                    onClick={() => fetchByCategory(category)}
+                  >
+                    {category}
+                  </p>
+                ))}
+              </div>
+            )}
+
+            <div className="bg-white py-2 px-3 border border-gray-200 flex justify-between">
+              <h1 className="font-semibold text-sm">Sizes</h1>
+              <p onClick={() => toggleVisibility("sizes")}>
+                {visibility.sizes ? <FaAngleUp /> : <FaAngleDown />}
+              </p>
+            </div>
+            {!visibility.sizes && (
+              <div className="bg-white border border-gray-200">
+                {Array.from(
+                  new Set(productType.map((product) => product.size))
+                ).map((size) => (
+                  <p
+                    className="text-xs py-2 px-6 cursor-pointer hover:bg-gray-200"
+                    key={size}
+                    onClick={() => fetchBySize(size)}
+                  >
+                    {size} Ton
+                  </p>
+                ))}
+              </div>
+            )}
+            <div className="bg-white py-2 px-3 border border-gray-200  justify-between">
+                <p className="">
+                  NPR {priceRange[0]} - NPR {priceRange[1]}
+                </p>
+              <input type="range" className="w-full" />
             </div>
           </div>
+
           <div className="lg:col-span-3 md:col-span-1">
             <h1 className="text-[#888888] font-semibold text-xl p-4">{type}</h1>
             <div className="flex justify-between">
@@ -99,13 +151,13 @@ export default function AirConditioner() {
               {filteredProduct.map((productDetail) =>
                 productDetail.discount === 0 ? (
                   <CatProdDetailCard
-                    key={productDetail.name}
+                    key={productDetail.$id}
                     productDetail={productDetail}
                   />
                 ) : (
                   <DiscountedProductDetailCard
                     productDetail={productDetail}
-                    key={productDetail.name}
+                    key={productDetail.$id}
                   />
                 )
               )}
