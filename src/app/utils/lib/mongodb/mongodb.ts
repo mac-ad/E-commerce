92fad2a737ae08e9463b421786b82d070f1d7731
mac-ad <macad626@gmail.com>
2025-03-18 15:28:40 +0545

    fixed uri

--- src/app/utils/lib/mongodb/mongodb.ts
+++ src/app/utils/lib/mongodb/mongodb.ts
@@ -1,10 +1,11 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI
-// process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI as string;
+const DB_NAME = process.env.DB_NAME as string
 
-if (!MONGODB_URI)
+if (!MONGODB_URI || !DB_NAME)
     throw new Error("Missing mongo uri")
+
 
 interface MongooseCache {
     conn: Mongoose | null;
@@ -15,13 +16,14 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 
 export async function connectToDb(): Promise<any> {
-    if(!MONGODB_URI) throw new Error("Missing mongo uri")
+    if(!MONGODB_URI || !DB_NAME) throw new Error("Missing mongo uri or db name")
     try {
         if (cached.conn) return cached.conn;
-        
 
         if (!cached.promise) {
-            cached.promise = mongoose.connect(MONGODB_URI)
+            cached.promise = mongoose.connect(MONGODB_URI,{
+                dbName:DB_NAME
+            })
         }
 
         cached.conn = await cached.promise;
