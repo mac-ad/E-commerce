e0eb7c548a7b2e33fc345e7378e962ec9b051183
mac-ad <macad626@gmail.com>
2025-03-19 19:06:16 +0545

    fixed images static

--- src/app/utils/lib/multer.ts
+++ src/app/utils/lib/multer.ts
@@ -3,21 +3,21 @@ import path from 'path';
 import fs from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 
-// Configure Multer storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadDir = path.join(process.cwd(), "public/uploads");
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir, { recursive: true });
-        }
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
-});
+// // Configure Multer storage
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         const uploadDir = path.join(process.cwd(), "@/uploads");
+//         if (!fs.existsSync(uploadDir)) {
+//             fs.mkdirSync(uploadDir, { recursive: true });
+//         }
+//         cb(null, uploadDir);
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
 const uploadAllFiles = async ({
     files,
@@ -31,7 +31,7 @@ const uploadAllFiles = async ({
         try {
             const bytes = await file.arrayBuffer();
             const buffer = Buffer.from(bytes);
-            const uploadDir = path.join(process.cwd(), "public/uploads");
+            const uploadDir = path.join(process.cwd(), "src/uploads");
 
             // Check if directory exists and create if needed
             if (!await fs.promises.access(uploadDir).then(() => true).catch(() => false)) {
