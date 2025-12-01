cb859c274fbf20376697072975062233b5984f59
ashishSand <ashish@sandconsole.com>
2025-12-01 16:14:20 +0545

    fixed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 // next.config.js
 module.exports = {
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-    images: {
-      unoptimized: true,
-      dangerouslyAllowSVG: true,
-      remotePatterns: [
-      ],
-    },
-  };
-  
+  output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+    ],
+  },
+};
